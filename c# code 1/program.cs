794ce50e5737dc724637d3c4361e7748a6d9a090
arkoc <artiom.kocharov@gmail.com>
2024-09-26 10:02:52 +0200

    Add C# Code 1

---
+++ c# code 1/program.cs
@@ -0,0 +1,8 @@
+﻿/*
+Import namespace called system that has many classes
+and useful functions (Without it we'd have to type
+System.Console.Write)
+*/
+using System;
+using System.Globalization;
+using System.Text;
