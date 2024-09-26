eb8c6e9259a58d13667a8d66c93e6fc95f0b99e9
arkoc <artiom.kocharov@gmail.com>
2024-09-26 10:34:06 +0200

    2024-09-26

--- c# code 1/program.cs
+++ c# code 1/program.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿﻿/*
 Import namespace called system that has many classes
 and useful functions (Without it we'd have to type
 System.Console.Write)
@@ -6,3 +6,16 @@ System.Console.Write)
 using System;
 using System.Globalization;
 using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is your name?" );
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
